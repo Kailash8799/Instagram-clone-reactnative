413ff4e1288ddfa366ce421ffa4d346b2136e7ee
kailash8799 <kailashrajput8799@gmail.com>
2023-07-17 23:51:05 +0530

    added reels

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -32,7 +32,7 @@ const Explore = () => {
           userimage:
             "https://plus.unsplash.com/premium_photo-1661448623542-eb6ec42ec5b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
           postlink:
-            "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/350363435_2008614696146497_5054284732318692261_n.jpg?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=Sykr0ifCBYkAX_JNSIx&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzExNDY0NzA4MTc2MjEyNzIxMQ%3D%3D.2-ccb7-5&oh=00_AfDE9NGrH9jB6jqkMnxOysBeC-CeuZd2fI8o0A8Oy093mg&oe=64BB4476&_nc_sid=b41fef",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -43,7 +43,7 @@ const Explore = () => {
           username: "nilesh9106",
           userimage: "https://avatars.githubusercontent.com/u/97864081?v=4",
           postlink:
-            "https://plus.unsplash.com/premium_photo-1661320875783-bee87f406d17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/343757043_1285409619056241_5894221887533366420_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=SxYQHRImH0gAX-xgWEG&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzA5MDk0NDg1ODkzNDc4Njg4OA%3D%3D.2-ccb7-5&oh=00_AfDJ6dVokppI7Nbv2Wwe6vYgn93wd2HhL3bw9XM-R2KfRw&oe=64BA737A&_nc_sid=b41fef",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -55,7 +55,7 @@ const Explore = () => {
           userimage:
             "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/351708358_6135564289885687_6519741987364001685_n.webp?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Q-fOqnY4DRIAX_QCdDP&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzExOTkyNzczOTI2NTIwNDg5Ng%3D%3D.2-ccb7-5&oh=00_AfDlH6Qrp5CiFaDBUY0zRcSMYmYXXsvqNIJMrDtczU2CJw&oe=64BA9DFD&_nc_sid=ee9879",
           postlink:
-            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/351708358_6135564289885687_6519741987364001685_n.webp?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Q-fOqnY4DRIAX_QCdDP&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzExOTkyNzczOTI2NTIwNDg5Ng%3D%3D.2-ccb7-5&oh=00_AfDlH6Qrp5CiFaDBUY0zRcSMYmYXXsvqNIJMrDtczU2CJw&oe=64BA9DFD&_nc_sid=ee9879",
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/351758734_990011895675106_8279348446799757155_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=_UHhcmE-o80AX-cOghR&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzExOTgwOTcyMTE0NDY2NTkyMg%3D%3D.2-ccb7-5&oh=00_AfBWS8rQvISogqBrm1XvN5uAoTKC-fEfmaLbvobG1KRsAA&oe=64BA4249&_nc_sid=b41fef",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -96,7 +96,7 @@ const Explore = () => {
           userimage:
             "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-19/344759869_645444740930076_6013201269205349713_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=27H5STwBwBAAX_dvLYE&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfA9SoRxhKIN7S-YoxF1v1tA7ZID978NEK0gnFht2Iae3A&oe=64B9BC42&_nc_sid=8b3546",
           postlink:
-            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-19/344759869_645444740930076_6013201269205349713_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=27H5STwBwBAAX_dvLYE&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfA9SoRxhKIN7S-YoxF1v1tA7ZID978NEK0gnFht2Iae3A&oe=64B9BC42&_nc_sid=8b3546",
+            "https://instagram.famd15-1.fna.fbcdn.net/v/t51.2885-15/335675458_1920013738367366_3086674298281480561_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.famd15-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=_0Bt6IgAFnkAX-kHlCk&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzA1ODI2MjMzOTE2OTI0MTM1OA%3D%3D.2-ccb7-5&oh=00_AfDyVSDSsm4LMQ15XH4yuQyv-DGVJAX9OD1WZsVAy5epvQ&oe=64BADF64&_nc_sid=b41fef",
           caption:
             "Hey buddy how are you and fine jo bhi hoga vo bhi dekha jayega",
           time: "",
@@ -109,7 +109,7 @@ const Explore = () => {
           userimage:
             "https://plus.unsplash.com/premium_photo-1661448623542-eb6ec42ec5b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
           postlink:
-            "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/318688420_5982345541777188_6142899702325291537_n.jpg?stp=dst-jpg_e35_p480x480&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=1Ax6k7TS-VcAX8yulOx&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=Mjk4OTE3NDY2MzU5NTk2NjUwOA%3D%3D.2-ccb7-5&oh=00_AfAKThkZu9ZHVEcPO1ELcTLWuFI_a1RWMYdX7RdPQE_ptA&oe=64BB4C5C&_nc_sid=b41fef",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -121,7 +121,19 @@ const Explore = () => {
           userimage:
             "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/351708358_6135564289885687_6519741987364001685_n.webp?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Q-fOqnY4DRIAX_QCdDP&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzExOTkyNzczOTI2NTIwNDg5Ng%3D%3D.2-ccb7-5&oh=00_AfDlH6Qrp5CiFaDBUY0zRcSMYmYXXsvqNIJMrDtczU2CJw&oe=64BA9DFD&_nc_sid=ee9879",
           postlink:
-            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/351708358_6135564289885687_6519741987364001685_n.webp?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Q-fOqnY4DRIAX_QCdDP&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzExOTkyNzczOTI2NTIwNDg5Ng%3D%3D.2-ccb7-5&oh=00_AfDlH6Qrp5CiFaDBUY0zRcSMYmYXXsvqNIJMrDtczU2CJw&oe=64BA9DFD&_nc_sid=ee9879",
+            "https://instagram.famd15-1.fna.fbcdn.net/v/t51.2885-15/296964295_1216453879139799_8559805819307354189_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=BGYhw955p_MAX8L22hg&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=Mjg5NTA5NzA4NTEwMDg4NDg5MA%3D%3D.2-ccb7-5&oh=00_AfDg1DOVrfXv7ooW8B0Pt9dml8Ol9qBVy9tdNqWiGFPnNw&oe=64BAB3D8&_nc_sid=b41fef",
+          caption: "Oh no yes ",
+          time: "",
+          isvideo: false,
+          hashtags: ["lover", "friends"],
+        },
+        {
+          id: "asdfdfghfcvvdccvxvb",
+          username: "dilip231",
+          userimage:
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/353577627_205337085800050_582486605235670258_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=3NU4VQ2yP2QAX-QzMib&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzEyNDk2MzM4NzQzNjY5OTk5NA%3D%3D.2-ccb7-5&oh=00_AfDhZ5s8UMovYRNPGrQtiYJlgxBh6wUg2atwHvSMv52GJw&oe=64B996FC&_nc_sid=ee9879",
+          postlink:
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/353577627_205337085800050_582486605235670258_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=3NU4VQ2yP2QAX-QzMib&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzEyNDk2MzM4NzQzNjY5OTk5NA%3D%3D.2-ccb7-5&oh=00_AfDhZ5s8UMovYRNPGrQtiYJlgxBh6wUg2atwHvSMv52GJw&oe=64B996FC&_nc_sid=ee9879",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -133,19 +145,7 @@ const Explore = () => {
           userimage:
             "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/360029942_130737243392370_4232524164229805425_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=3ETDLWNuNwcAX99YILh&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzE0NjUzMjc5NTQxMzQ2MTEzOQ%3D%3D.2-ccb7-5&oh=00_AfAK-8nn8gGX-AZNNXrHsyaU_sa5zQSWbyXvXUdAwNOXZA&oe=64BA1BA3&_nc_sid=ee9879",
           postlink:
-            "https://instagram.famd15-1.fna.fbcdn.net/v/t51.2885-15/358349201_3541860762801080_7487653754704439834_n.jpg?stp=dst-jpg_e35_p480x480&_nc_ht=instagram.famd15-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=0iisJLymVc8AX9ePPed&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzE0MjA2MDY2MzU4NTQzMzg4OA%3D%3D.2-ccb7-5&oh=00_AfA4IckcMwJT08O42a0zOSyxeqd6haypdfj8CCJZxidbdg&oe=64BA6744&_nc_sid=ee9879",
-          caption: "Oh no yes ",
-          time: "",
-          isvideo: false,
-          hashtags: ["lover", "friends"],
-        },
-        {
-          id: "asdfdfghfcvvdccvxvb",
-          username: "dilip231",
-          userimage:
-            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/360029942_130737243392370_4232524164229805425_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=3ETDLWNuNwcAX99YILh&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzE0NjUzMjc5NTQxMzQ2MTEzOQ%3D%3D.2-ccb7-5&oh=00_AfAK-8nn8gGX-AZNNXrHsyaU_sa5zQSWbyXvXUdAwNOXZA&oe=64BA1BA3&_nc_sid=ee9879",
-          postlink:
-            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/350658370_183658280999369_8154665927624677857_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=omi3xSITUDEAX8zGqce&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzExNzU4MjQxODE5MDQ1MzgxNQ%3D%3D.2-ccb7-5&oh=00_AfB_5QfvaAI5Qvw0_2Zr9RqOGnBIrIIXWdwIe-RZkv4qLA&oe=64BAA471&_nc_sid=ee9879",
+            "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-15/336247062_247457004289545_4121003916927279644_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=C5mCjfvGiTAAX9c9IUl&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzA1ODU2MTU1NDI5ODYzODE1Nw%3D%3D.2-ccb7-5&oh=00_AfDIvkn_QFbDvhYDVs3176P1hhvxdpUZ1eT6S5IWOAF8og&oe=64BA536D&_nc_sid=b41fef",
           caption: "Oh no yes ",
           time: "",
           isvideo: false,
@@ -278,7 +278,7 @@ const Explore = () => {
             >
               {item?.id === 0 ? (
                 <View
-                  className="grid flex-row  flex-wrap grid-cols-3"
+                  className="grid flex-row flex-wrap grid-cols-3"
                   key={key}
                 >
                   {(item?.images).map((obj, key) => {
--- src/components/Reels.js
+++ src/components/Reels.js
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  useColorScheme,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { StyleSheet, Button } from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { Video, ResizeMode } from "expo-av";
+
+let width = Dimensions.get("screen").width;
+let height = Dimensions.get("screen").height;
 
 const Reels = () => {
+  const colorScheme = useColorScheme();
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  const [isMuted, setIsMuted] = React.useState(false);
   return (
-    <View>
-      <Text>Reels</Text>
+    <View
+      style={{
+        position: "relative",
+        height: height,
+        width: width,
+        backgroundColor: colorScheme === "dark" ? "black" : "#ffffff",
+      }}
+    >
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 15,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          right: 15,
+          zIndex: 500,
+        }}
+      >
+        <Text className="text-xl font-bold text-black dark:text-white">
+          Reels
+        </Text>
+        <TouchableOpacity activeOpacity={0.6}>
+          <Feather
+            name="camera"
+            size={24}
+            color={colorScheme === "dark" ? "#ffffff" : "black"}
+          />
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        <TouchableOpacity
+          onPress={()=>{setIsMuted(!isMuted)}}
+          activeOpacity={0.9}
+          onPressOut={()=>{
+            status.isPlaying
+              ? null
+              : video.current.playAsync();
+          }}
+          onLongPress={() => {
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync();
+          }}
+          style={[styles.container, { width: width, height: height }]}
+        >
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+            }}
+            useNativeControls={false}
+            isMuted={isMuted}
+            resizeMode={ResizeMode.COVER}
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+            onReadyForDisplay={() => {
+              video?.current?.playAsync();
+            }}
+            isLooping={true}
+          />
+        </TouchableOpacity>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default Reels
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  video: {
+    alignSelf: "center",
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Reels;
