88bd0bcee63a5cb308672167b655fffb74af54fb
kailash8799 <kailashrajput8799@gmail.com>
2023-07-18 21:38:01 +0530

    design a reels section

--- src/components/Reels.js
+++ src/components/Reels.js
@@ -5,25 +5,220 @@ import {
   useColorScheme,
   TouchableOpacity,
   Dimensions,
+  FlatList,
 } from "react-native";
 import { StyleSheet, Button } from "react-native";
 import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { Video, ResizeMode } from "expo-av";
+import OneReels from "./subcomponent/OneReels";
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
 
 let width = Dimensions.get("screen").width;
 let height = Dimensions.get("screen").height;
 
 const Reels = () => {
   const colorScheme = useColorScheme();
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
-  const [isMuted, setIsMuted] = React.useState(false);
+  const totalreels = [
+    {
+      id: "asdfghfdcxvb",
+      username: "kailash8799",
+      userimage:
+        "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-19/344759869_645444740930076_6013201269205349713_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=27H5STwBwBAAX_dvLYE&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfA9SoRxhKIN7S-YoxF1v1tA7ZID978NEK0gnFht2Iae3A&oe=64B9BC42&_nc_sid=8b3546",
+      postlink:
+        "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      caption: "Hey buddy how are you and fine jo bhi hoga vo bhi dekha jayega",
+      time: "",
+      isvideo: false,
+      hashtags: ["posts", "love"],
+    },
+    {
+      id: "asdfghfcvvdcxvb",
+      username: "dasharath07",
+      userimage:
+        "https://plus.unsplash.com/premium_photo-1661448623542-eb6ec42ec5b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      postlink:
+        "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["buddy", "janu"],
+    },
+    {
+      id: "asdfghfcvvdccvxvb",
+      username: "nilesh9106",
+      userimage: "https://avatars.githubusercontent.com/u/97864081?v=4",
+      postlink:
+        "https://plus.unsplash.com/premium_photo-1661320875783-bee87f406d17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["baby", "india"],
+    },
+    {
+      id: "asdfdcvvvfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdfcvsfbhbfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdffsdrstgfhdfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "ascvbdfddsdfhfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "axcvbnsdfwszhjkdfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdfgbsswtrhfcbvbvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdcxvfdfghfcxvbcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdfghfcvvdccxcxcxqwvxvbxcvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdvbnnmggdfghfbpicvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "ascbvcvbdfdfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "asdfdfghcxfcffccvvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+    {
+      id: "avbv54vghsdfdfghfcvvdccvxvb",
+      username: "dilip231",
+      userimage:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      postlink:
+        "https://media.licdn.com/dms/image/D4D03AQHsFost9OYZNQ/profile-displayphoto-shrink_800_800/0/1686843781093?e=1694044800&v=beta&t=LpPVVu8oyUdsnVeSz8lUAcfZArXQTfevgZQIYeUOabY",
+      caption: "Oh no yes ",
+      time: "",
+      isvideo: false,
+      hashtags: ["lover", "friends"],
+    },
+  ];
+  // const totalreels = [
+  //   {
+  //     id: "asdfghfdcxvb",
+  //     username: "kailash8799",
+  //     userimage:
+  //       "https://instagram.famd15-2.fna.fbcdn.net/v/t51.2885-19/344759869_645444740930076_6013201269205349713_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.famd15-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=27H5STwBwBAAX_dvLYE&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfA9SoRxhKIN7S-YoxF1v1tA7ZID978NEK0gnFht2Iae3A&oe=64B9BC42&_nc_sid=8b3546",
+  //     postlink:
+  //       "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+  //     caption: "Hey buddy how are you and fine jo bhi hoga vo bhi dekha jayega",
+  //     time: "",
+  //     isvideo: false,
+  //     hashtags: ["posts", "love"],
+  //   },
+  // ];
   return (
     <View
       style={{
         position: "relative",
-        height: height,
+        height: height - 68,
         width: width,
         backgroundColor: colorScheme === "dark" ? "black" : "#ffffff",
       }}
@@ -52,59 +247,38 @@ const Reels = () => {
           />
         </TouchableOpacity>
       </View>
-      <ScrollView>
-        <TouchableOpacity
-          onPress={()=>{setIsMuted(!isMuted)}}
-          activeOpacity={0.9}
-          onPressOut={()=>{
-            status.isPlaying
-              ? null
-              : video.current.playAsync();
-          }}
-          onLongPress={() => {
-            status.isPlaying
-              ? video.current.pauseAsync()
-              : video.current.playAsync();
-          }}
-          style={[styles.container, { width: width, height: height }]}
-        >
-          <Video
-            ref={video}
-            style={styles.video}
-            source={{
-              uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
-            }}
-            useNativeControls={false}
-            isMuted={isMuted}
-            resizeMode={ResizeMode.COVER}
-            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-            onReadyForDisplay={() => {
-              video?.current?.playAsync();
-            }}
-            isLooping={true}
-          />
-        </TouchableOpacity>
-      </ScrollView>
+      <FlatList
+        data={totalreels}
+        pagingEnabled={true}
+        renderItem={({ item, ind }) => {
+          return (
+            <OneReels
+              key={ind}
+              id={item?.id}
+              username={item?.username}
+              userimage={item?.userimage}
+              postlink={item?.postlink}
+              caption={item?.caption}
+              time={item?.time}
+              hashtags={item?.hashtags}
+              song={"Baris barsa pani chham dfdv dfvdfv fvdf vds vdv dvd d d d  chham"}
+              mensition={["nepal","kailash"]}
+              totallikes={200}
+              totalcomment={343}
+            />
+          );
+        }}
+        keyExtractor={(item) => item.id}
+        snapToAlignment="center"
+        decelerationRate={"fast"}
+
+        // snapToInterval={Dimensions.get("window").height-71}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
+      <View style={{height:20}}></View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "black",
-  },
-  video: {
-    alignSelf: "center",
-    width: 320,
-    height: 200,
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default Reels;
---
+++ src/components/subcomponent/OneReels.jsx
@@ -0,0 +1,290 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  useColorScheme,
+} from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Video, ResizeMode } from "expo-av";
+
+import { StyleSheet } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
+let width = Dimensions.get("screen").width;
+let height = Dimensions.get("screen").height;
+
+const OneReels = ({
+  userimage,
+  username,
+  caption,
+  hashtags,
+  song,
+  mensition,
+  totallikes,
+  totalcomment
+}) => {
+  
+  const colorScheme = useColorScheme();
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  const [isMuted, setIsMuted] = React.useState(false);
+  const [hasLiked, sethasLiked] = useState(false);
+  const [hasbookmark, sethasbookmark] = useState(false);
+
+  return (
+    <View style={{ position: "relative", height: height - 87, width: width }}>
+      <TouchableOpacity
+        onPress={() => {
+          setIsMuted(!isMuted);
+        }}
+        activeOpacity={0.9}
+        onPressOut={() => {
+          status.isPlaying ? null : video.current.playAsync();
+        }}
+        onLongPress={() => {
+          status.isPlaying
+            ? video.current.pauseAsync()
+            : video.current.playAsync();
+        }}
+        style={[styles.container, { width: width, height: height,backgroundColor:colorScheme==='dark'?"black":"#fff" }]}
+      >
+        <Video
+          ref={video}
+          style={styles.video}
+          source={require("../../../assets/v2.mp4")}
+          useNativeControls={false}
+          isMuted={isMuted}
+          resizeMode={ResizeMode.COVER}
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+          onReadyForDisplay={() => {
+            video?.current?.playAsync();
+          }}
+          isLooping={false}
+        />
+        <View
+          style={{
+            position: "absolute",
+            bottom: 2,
+            left: 2,
+            zIndex: 100,
+            width: width - 100,
+          }}
+        >
+          <View className="justify-center px-3 space-y-1">
+            <View className="flex flex-row items-center space-x-3">
+              <TouchableOpacity
+                activeOpacity={0.7}
+                className="w-[35px] items-center rounded-full cursor-pointer"
+              >
+                <View className="relative items-center w-full overflow-hidden rounded-full aspect-square">
+                  <Image
+                    source={{ uri: userimage }}
+                    className="w-full h-full transition rounded-full hover:scale-105"
+                    resizeMode="cover"
+                    style={{
+                      resizeMode: "cover",
+                      backgroundColor: "orange",
+                      borderRadius: 100,
+                      height: "100%",
+                      width: "100%",
+                      zIndex: 100,
+                    }}
+                  />
+                </View>
+              </TouchableOpacity>
+              <View className="flex flex-row items-center">
+                <Text className="dark:text-white text-slate-300">{username}</Text>
+                <TouchableOpacity activeOpacity={0.7}>
+                  <Image
+                    source={require("../../../assets/bluetick.png")}
+                    resizeMode="cover"
+                    style={{
+                      resizeMode: "cover",
+                      borderRadius: 100,
+                      height: 18,
+                      width: 18,
+                      zIndex: 100,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View>
+              <Text className="dark:text-white text-slate-300">{caption}</Text>
+            </View>
+            <View className="flex flex-row flex-wrap ">
+              {hashtags.map((item, ind) => {
+                return (
+                  <TouchableOpacity activeOpacity={0.7} key={ind}>
+                    <Text className="text-blue-600">
+                      #{item}
+                      {"  "}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+            <View className="flex flex-row items-center justify-between pb-1 space-x-2">
+              <View className="flex flex-row items-center justify-center px-3 py-1 space-x-2 rounded-full bg-slate-800/60 ">
+              <Feather name="music" size={15} color="white" />
+                <Text className="flex-wrap dark:text-white text-slate-300">
+                  {song?.slice(0, 16) + "..."}
+                </Text>
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                className="flex flex-row items-center justify-center px-3 py-1 space-x-2 rounded-full bg-slate-800/60 "
+              >
+                <Ionicons
+                  name="ios-person"
+                  size={15}
+                  color="white"
+                  style={{ opacity: 100 }}
+                />
+                <Text className="opacity-100 dark:text-white text-slate-300">
+                  {mensition?.length + " people"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            opacity: 100,
+            height: 210,
+            width: 50,
+            right: 2,
+            bottom: 60,
+          }}
+        >
+          <View style={{ elevation: 0.3, zIndex: 10 }} className="items-center">
+            <View
+              style={{ elevation: 0.3, zIndex: 10 }}
+              className="items-center space-y-5"
+            >
+              <View className="items-center justify-center text-center">
+                {hasLiked ? (
+                  <TouchableOpacity
+                    onPress={() => {
+                      sethasLiked(!hasLiked);
+                    }}
+                  >
+                    <AntDesign
+                      style={{ elevation: 0.3, zIndex: 10 }}
+                      name="heart"
+                      size={26}
+                      color="red"
+                    />
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity
+                    onPress={() => {
+                      sethasLiked(!hasLiked);
+                    }}
+                  >
+                    <Feather
+                      style={{ elevation: 0.3, zIndex: 10 }}
+                      name="heart"
+                      size={26}
+                      color={colorScheme==='dark'?"#fff":"rgb(203 213 225)"}
+                    />
+                  </TouchableOpacity>
+                )}
+                <Text className="text-center dark:text-white text-slate-300">{totallikes}</Text>
+              </View>
+              <View>
+                <TouchableOpacity>
+                  <Feather
+                    style={{ elevation: 0.3, zIndex: 10, fontSize: 25, top: 1 }}
+                    name="navigation"
+                    color={colorScheme==='dark'?"#fff":"rgb(203 213 225)"}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View className="items-center justify-center text-center">
+                <TouchableOpacity>
+                  <FontAwesome5
+                    style={{ elevation: 0.3, zIndex: 10 }}
+                    name="comment"
+                    size={26}
+                    color={colorScheme==='dark'?"#fff":"rgb(203 213 225)"}
+                  />
+                </TouchableOpacity>
+                <Text className="text-center dark:text-white text-slate-300">{totalcomment}</Text>
+              </View>
+              <View>
+                <TouchableOpacity>
+                  <Entypo
+                    name="dots-three-vertical"
+                    size={22}
+                    color={colorScheme==='dark'?"#fff":"rgb(203 213 225)"}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            opacity: 100,
+            height: 37,
+            width: 37,
+            right: 10,
+            bottom: 2,
+            borderRadius: 8,
+          }}
+        >
+          <TouchableOpacity
+            activeOpacity={0.7}
+            className="items-center cursor-pointer "
+          >
+            <View className="relative items-center w-full overflow-hidden aspect-square">
+              <Image
+                source={{ uri: userimage }}
+                className="w-full h-full transition rounded-lg"
+                resizeMode="cover"
+                style={{
+                  resizeMode: "cover",
+                  backgroundColor: "orange",
+                  height: "100%",
+                  width: "100%",
+                  zIndex: 100,
+                }}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  video: {
+    alignSelf: "center",
+    width: "100%",
+    height: "100%",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default OneReels;
